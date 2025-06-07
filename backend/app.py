62d24b46957f03367484c7d4d0c906ea28011f3d
Manikandan17S <manikandankid17@gmail.com>
2025-06-07 15:27:04 +0530

    Your update message here

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,11 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
-
+# remove or comment out app.run(), Render uses Gunicorn or similar automatically
 app = Flask(__name__)
 CORS(app)  # Allow requests from frontend
 
 @app.route('/api/hello', methods=['GET'])
 def hello():
     return jsonify({'message': 'Hello from Flask backend 👋'})
-# remove or comment out app.run(), Render uses Gunicorn or similar automatically
 if __name__ == '__main__':
     app.run(debug=True)
